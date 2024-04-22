30b701e1677a8b3af3829115aa4f4fdd5a09dd7b
HasanHafizurRahman <hasanshanto922@gmail.com>
2024-04-22 17:56:52 +0600

    typescript interface

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,12 +1,13 @@
 "use client"
+import { CustomButtonProps } from "@/types";
 import Image from "next/image";
 
-const CustomButton = ({title, containerStyles}) => {
+const CustomButton = ({title, containerStyles, handleClick}: CustomButtonProps) => {
     return (
         <button
             disabled={false}
             type={"button"}
-            className={`custom-btn`}
+            className={`custom-btn ${containerStyles}`}
             onClick={() => { }}
         >
             <span className={`flex-1`}>
