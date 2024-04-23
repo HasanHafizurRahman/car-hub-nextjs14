4683fdc34ebb18b29e81e374735b65a8d30bcb22
HasanHafizurRahman <hasanshanto922@gmail.com>
2024-04-23 17:48:06 +0600

    car catelog

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
-import { Hero } from "@/components";
+import { CustomFilter, Hero, SearchBar } from "@/components";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
-     <Hero />
+      <Hero />
+
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catelogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+
+        <div className="home__filters">
+          <SearchBar />
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
--- components/index.ts
+++ components/index.ts
@@ -2,7 +2,9 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
 
 export {
-    Hero, CustomButton, Navbar, Footer,
+    Hero, CustomButton, Navbar, Footer, SearchBar, CustomFilter
 }
